92b7abe253006f88b9eaa0f5bc64249e1817ad09
mvs.meah <mvs.meah@yahoo.com>
2021-11-16 12:02:31 +0800

    added intents

--- app/src/main/java/com/example/sandoval/HomeProducts.java
+++ app/src/main/java/com/example/sandoval/HomeProducts.java
@@ -2,13 +2,71 @@ package com.example.sandoval;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeProducts extends AppCompatActivity {
+
+    ImageButton homeBtn,addProds,searchByIdProds, viewAllProds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_products);
+
+        homeBtn = findViewById(R.id.homeBtn2);
+        addProds = findViewById(R.id.addBtn);
+        searchByIdProds = findViewById(R.id.searchByIdBtn);
+        viewAllProds = findViewById(R.id.viewAllBtn);
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                home();
+            }
+        });
+
+        addProds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProd();
+            }
+        });
+
+        searchByIdProds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchID();
+            }
+        });
+
+        viewAllProds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewAllProd();
+            }
+        });
+    }
+
+    public void home(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void addProd(){
+        Intent intent = new Intent(getApplicationContext(), AddProducts.class);
+        startActivity(intent);
+    }
+
+    public void searchID(){
+        Intent intent = new Intent(getApplicationContext(), SearchProductID.class);
+        startActivity(intent);
+    }
+
+    public void viewAllProd(){
+        Intent intent = new Intent(getApplicationContext(), ViewAllProducts.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/sandoval/SearchProductID.java
@@ -0,0 +1,14 @@
+package com.example.sandoval;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchProductID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_product_id);
+    }
+}
